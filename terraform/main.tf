943e6d05d9ea7a8f83bf7e8345fbb8f1396188a2
Luis de Frutos <luisdefrutos@outlook.com>
2025-07-01 12:03:43 +0200

    Subida limpia sin archivos grandes

---
+++ terraform/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.0"
+    }
+  }
+}
+
+#data "azurerm_client_config" "current" {}
+
+#provider "azurerm" {
+ # features {}
+
+  #subscription_id = data.azurerm_client_config.current.subscription_id
+  #tenant_id       = data.azurerm_client_config.current.tenant_id
+#}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-cp2"
+  location = "westeurope"
+  tags     = { environment = "casopractico2" }
+}
+provider "azurerm" {
+  features {}
+  use_cli = true
+   subscription_id = "c8517f2b-2938-4f18-8587-38039f81fb24"
+
+}
